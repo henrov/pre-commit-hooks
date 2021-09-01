c9183ae4024cd83985596d1716a98eac1259e0a4
Richard Kooijman <rkooijman@quicknet.nl>
2021-09-01 21:52:13 +0200

    cleaned up requirements and made setup requirements.txt aware

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
+import distutils.text_file
+from pathlib import Path
 
-setup()
+
+def _parse_requirements(filename):
+    """Return requirements from requirements file."""
+    # Ref: https://stackoverflow.com/a/42033122/
+    return distutils.text_file.TextFile(filename=str(Path(__file__).with_name(filename))).readlines()
+
+
+setup(install_requires=_parse_requirements('requirements.txt'))
