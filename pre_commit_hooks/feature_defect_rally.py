a16c98a8667e8c0a430a4511d16d608af3b10642
Henro Veijer <henro.veijer@inergy.nl>
2022-06-23 13:00:47 +0200

    Force message on screen

--- pre_commit_hooks/feature_defect_rally.py
+++ pre_commit_hooks/feature_defect_rally.py
@@ -136,10 +136,10 @@ def main() -> None:
         
         
         if not match:	
-            print("-----------------------------------------------------------------------------------")
-            print("No valid user story or defect ID in the branch name, changelog will not be amended.")
-            print("-----------------------------------------------------------------------------------")
-            flush()
+            print("-----------------------------------------------------------------------------------", flush=True)
+            print("No valid user story or defect ID in the branch name, changelog will not be amended.", flush=True)
+            print("-----------------------------------------------------------------------------------", flush=True)
+            
             return 0
             
         formatted_id_list = [match.group(2)]
